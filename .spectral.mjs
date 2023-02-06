df4d987b66b0162507455430136f3621dcf251f0
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2023-02-06 17:57:06 -0600

    Add Demo Style Guide.

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MjgyOA/spectral.js?branch_slug=main&token=a4954c1b-9e50-4149-a4be-516255266a32";
+export default { extends: ruleset };
